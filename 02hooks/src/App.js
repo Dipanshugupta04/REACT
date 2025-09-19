e6ab9cce0b4403514d68e51e8ca642a1b9b03688
Dipanshu <dipanshugupta9462@gmail.com>
2025-09-19 21:57:53 +0530

    Make a 02hooks

--- 02hooks/src/App.js
+++ 02hooks/src/App.js
@@ -1,21 +1,36 @@
-
+import { useState } from 'react';
 import './App.css';
+ 
 
-let counter=0;
-function addvalue(){
-console.log("counter",counter);
-
-counter=counter+1;
-
-}
-function removevalue(){
-  console.log("counter",counter);
-  
-  counter=counter-1;
-  
-  }
 
 function App() {
+
+  let [counter,setCounter]=useState(0)
+
+  //let counter=0;
+  function addvalue(){
+  
+  
+    // console.log("counter",counter);
+  // setInterval(() => {
+  //   counter=counter+1;
+  //   setCounter(counter)
+  // }, 2000);
+
+
+  counter=counter+1;
+  setCounter(counter)
+  
+  }
+  function removevalue(){
+    console.log("counter",counter);
+    
+    counter=counter-1;
+    setCounter(counter)
+    
+    }
+
+
   return (
     <>
     <h1>Counter IS:{counter}</h1>
