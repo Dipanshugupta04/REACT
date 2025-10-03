befd99bbcfc80e33a139144fec4838f3ca059565
Dipanshu <dipanshugupta9462@gmail.com>
2025-10-03 17:28:45 +0530

    password generator

---
+++ password/src/App.js
@@ -0,0 +1,21 @@
+import { useState } from "react";
+
+
+function App() {
+
+  const [length,setlength]=useState(8) 
+  const [NumberAllowed,setnumberAllowed]=useState(false) 
+  const [characterAllowed,setcharacterAllowed]=useState(false) 
+  return (
+    <>
+    <h1 className="text-4xl text-center text-white">Psssword Generator</h1>
+    
+    <div>
+
+    </div>
+        </>
+  );
+
+}
+
+export default App;
